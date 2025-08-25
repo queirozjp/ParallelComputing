a6101825818d5cd3b1cffa2e5f13b8acd5c03634
queirozjp <160232317+queirozjp@users.noreply.github.com>
2025-08-25 10:50:25 +0000

    Aa

--- lab1/atv1.c
+++ lab1/atv1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <math.h>
 
 int main(){
     pid_t pid = fork();
--- lab1/atv2.c
+++ lab1/atv2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <math.h>
 
 int main(){
     int i, n = 4;
